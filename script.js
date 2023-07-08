a391d39e1946b58603b93445d7ca619bf1accd55
miwanoff <marina.iva.noffa@gmail.com>
2023-07-08 11:56:07 +0300

    Add Animation Effects

--- script.js
+++ script.js
@@ -13,4 +13,16 @@ $(function () {
     let item = `<div  class="item" style="background: url(${src});"></div>`;
     $(".owl-carousel.owl-theme").append(item);
   });
+
+  $(window).scroll(function () {
+    // якщо відбувається скролінг
+    $(".slideanim").each(function () {
+      // для кожного блоку з класом slideanim
+      let pos = $(this).offset().top; // считываем его координату по оси Y в окне браузера
+      if (pos < $(window).scrollTop() + 400) {
+        //якщо до верху сторінки залишається 400px,
+        $(this).addClass("slide"); //додаємо до блоку клас slide з анімацією
+      }
+    });
+  });
 });
